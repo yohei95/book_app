815585d8b134466d5823d8809fea0bec907ebb65
yohei95 <yuhi0002@gmail.com>
2021-03-18 08:34:30 +0900

    ログインページ

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
+    sleep 0.1
   end
 
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'name、email、password、password_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
+      end
     end
-  end
 
+<<<<<<< Updated upstream
   context '新規登録できないとき' do
     it 'nameが空では登録できない' do
       @user.name = ''
@@ -30,30 +32,50 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
     end
+=======
+    context '新規登録できないとき' do
+      it 'nameが空では登録できない' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Nameを入力してください')
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールを入力してください')
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+      end
+>>>>>>> Stashed changes
 
-    it 'パスワードが空では登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("パスワードを入力してください")
-    end
-    it 'パスワードが5文字以下では登録できない' do
-      @user.password = 'a0000'
-      @user.password_confirmation = 'a0000'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
-    end
+      it 'パスワードが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードを入力してください')
+      end
+      it 'パスワードが5文字以下では登録できない' do
+        @user.password = 'a0000'
+        @user.password_confirmation = 'a0000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+      end
 
-    it 'パスワードが存在してもpassword_confirmationが空では登録できない' do
-      @user.password_confirmation = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      it 'パスワードが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
+      end
+      it 'パスワードとパスワード（確認用）が一致しないと登録できない' do
+        @user.password = 'a00000'
+        @user.password_confirmation = 'b00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
+      end
     end
-    it 'パスワードとパスワード（確認用）が一致しないと登録できない' do
-      @user.password = 'a00000'
-      @user.password_confirmation = 'b00000'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
-    end
-  end
   end
 end
