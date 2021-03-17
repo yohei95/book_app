b3e690230514e3f8a86d7d0d3327f46338cc448c
yohei95 <yuhi0002@gmail.com>
2021-03-17 12:11:21 +0900

    静的ページの編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def full_title(page_title = '')  
+  def full_title(page_title = '')
     base_title = 'bookapp'
     if page_title.blank?
-      base_title 
+      base_title
     else
-      "#{page_title} - #{base_title}" 
+      "#{page_title} - #{base_title}"
     end
   end
 end
