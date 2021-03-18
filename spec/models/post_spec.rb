8dd1ba9f34f73150faa2fc8f5afd7b764622a0a5
yohei95 <yuhi0002@gmail.com>
2021-03-18 17:38:43 +0900

    postモデル単体テスト

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '投稿' do
+    context '新規投稿できるとき' do
+      it 'titleが存在すれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+  end
+end
