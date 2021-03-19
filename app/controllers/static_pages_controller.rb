b5bf2118853d4c63dca4c80578d3736bffd86f7b
yohei95 <yuhi0002@gmail.com>
2021-03-19 18:47:10 +0900

    投稿レイアウトの修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
-    if user_signed_in?
-      @post = current_user.posts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    else
-      @posts = Post.all
-    end
+    @posts = Post.page(params[:page])
+
   end
 
   def about
