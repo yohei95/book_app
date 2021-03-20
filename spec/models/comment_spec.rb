019a1738bfca8de60ceea6ce818032d2f98713ad
yohei95 <yuhi0002@gmail.com>
2021-03-20 10:39:25 +0900

    コメントモデルテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント' do
+    context 'コメントできるとき' do
+      it 'textが存在すれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+  end
 end
