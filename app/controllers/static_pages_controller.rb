2d77d1494430d3e2aef70e706c4f8ffa7f19186a
yohei95 <yuhi0002@gmail.com>
2021-03-21 08:48:22 +0900

    トップページ投稿表示件数を制限

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
-    @posts = Post.page(params[:page])
+    @posts = Post.page(params[:page]).order('created_at DESC').limit(5)
+    if user_signed_in?
+      @following_user_posts = Post.where(user_id: [current_user.following_ids]).order('created_at desc').limit(5)
+    end
 
   end
 
