c2f61bb1493763a8f0eebb2a29d1f30ec0ee9328
yohei95 <yuhi0002@gmail.com>
2021-03-20 14:08:08 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,16 @@ class CommentsController < ApplicationController
     redirect_to "/posts/#{comment.post.id}"
   end
 
+  def destroy
+
+    @comment = Comment.find(params[:post_id])
+    @post = @comment.post
+    if current_user.id == @comment.user_id
+      @comment.destroy
+    end
+    redirect_to post_url(@post)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
