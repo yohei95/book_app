61aba5bdd4b3ec7728ced927bc2639117f474943
yohei95 <yuhi0002@gmail.com>
2021-03-20 20:39:45 +0900

    ユーザー詳細画面にフォロー（解除）ボタン設置

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,15 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+
+  end
+end
