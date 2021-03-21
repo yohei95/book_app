fc8f6f9bc792d5b1afdc88f63bd28fd513ecd265
yohei95 <yuhi0002@gmail.com>
2021-03-21 15:13:06 +0900

    プロフィール画像編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email,:image)
   end
 
   def correct_user
