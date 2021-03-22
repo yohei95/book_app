45dcbfed1ff4982680a3e307ca785594e55eaf9d
yohei95 <yuhi0002@gmail.com>
2021-03-22 12:27:41 +0900

    簡単ログイン機能の実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = current_user
     @posts = Post.page(params[:page]).order('created_at DESC').limit(5)
     if user_signed_in?
       @following_user_posts = Post.where(user_id: [current_user.following_ids]).order('created_at desc').limit(5)
     end
-
   end
 
-  def about
-    @user = current_user
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+    end
+    sign_in user
+    redirect_to root_path
   end
 end
