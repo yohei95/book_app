ebc76fe8d69c46fa794f241394007bfee1b0bce9
yohei95 <yuhi0002@gmail.com>
2021-03-23 12:12:46 +0900

    postモデル単体テスト追記

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,9 +6,40 @@ RSpec.describe Post, type: :model do
 
   describe '投稿' do
     context '新規投稿できるとき' do
-      it 'title、planが存在すれば投稿できる' do
+      it 'title、memo、plan、reference、画像が存在すれば投稿できる' do
         expect(@post).to be_valid
       end
+      it 'memoは空でも投稿できる' do
+        @post.memo=''
+        expect(@post).to be_valid
+      end
+      it 'referenceは空でも投稿できる' do
+        @post.reference=''
+        expect(@post).to be_valid
+      end
+      it '画像は空でも投稿できる' do
+        @post.image=nil
+        expect(@post).to be_valid
+      end
+    end
+
+    context '新規投稿できないとき' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Titleを入力してください')
+      end
+      it 'planが空では投稿できない' do
+        @post.plan = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Planを入力してください')
+      end
+    end
+
+    it 'ユーザーが紐付いていなければ投稿できない' do
+      @post.user = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include('Userを入力してください')
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
-    sleep 0.1
   end
 
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
-      it 'name、email、password、password_confirmationが存在すれば登録できる' do
+      it 'name、email、password、password_confirmation、画像が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it '画像は空でも登録できる' do
+        @user.image = nil
         expect(@user).to be_valid
       end
     end
