60deea67b26725b824608e3259149afeadf260d5
yohei95 <yuhi0002@gmail.com>
2021-03-23 15:23:41 +0900

    PostコントローラRequestSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :completions, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one_attached :image
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+
+  before do
+    @post = FactoryBot.create(:post)
+  end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get post_path(@post)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのpostのmemoが存在する' do 
+      get post_path(@post)
+      expect(response.body).to include(@post.memo)
+    end
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+      get post_path(@post)
+      expect(response.body).to include('＜コメント一覧＞')
+    end
+  end 
+
+end
