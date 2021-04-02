36b371eafbc567801fefd36743ff10af6582d181
yohei95 <yuhi0002@gmail.com>
2021-04-03 00:18:30 +0900

    circletest

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
                                   # keys: ['~/.ssh/yohei.pem'] 
                                   keys: ['~/.ssh/id_rsa'] 
-#                                   keys:['~/.ssh/id_rsa_2839c35b5c092cb413b6ae10fd5eb360']  
+                                  #keys:['~/.ssh/id_rsa_2839c35b5c092cb413b6ae10fd5eb360']  
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
